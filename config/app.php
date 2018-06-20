835afa440e965e286ae4a978eeecd155a80105c1
Juan José Schiel <haanseen@gmail.com>
2018-06-20 20:33:59 -0300

    Agregadas dependencias para login

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         ProdeIAW\Providers\EventServiceProvider::class,
         ProdeIAW\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider::class,
     ],
 
     /*
@@ -210,6 +211,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
     ],
 
 ];
